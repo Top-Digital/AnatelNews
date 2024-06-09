bfb533322f811d8905d9094d9fdd3ccb698005a1
Adriano Alves Dal Cin Costa <adriano@ceviu.com.br>
2024-06-09 06:29:07 -0300

    Adicionei formatadores de HTML, mais variáveis de ambiente e cum coletor de erros

--- anatelNews.py
+++ anatelNews.py
@@ -1,22 +1,33 @@
 import os
+import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import mongoengine as me
-
-# Import the schemas
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
 from schemas.news_collection import NewsCollection
 from schemas.news_collection_not_posted import NewsCollectionNotPosted
 
 # Load environment variables
 load_dotenv()
 
+MONGO_URI = os.getenv('MONGO_URI')
+DB_NAME = os.getenv('DB_NAME')
+NEWS_COLLECTION = os.getenv('NEWS_COLLECTION')
+NEWS_COLLECTION_NOT_POSTED = os.getenv('NEWS_COLLECTION_NOT_POSTED')
 SHOW_BROWSER = os.getenv('SHOW_BROWSER') == 'true'
 
 # MongoDB connection
-me.connect(db='anatelnews', host='localhost', port=27017)
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+news_collection = db[NEWS_COLLECTION]
+news_collection_not_posted = db[NEWS_COLLECTION_NOT_POSTED]
+
+# drop collections
+news_collection.drop()
+news_collection_not_posted.drop()
 
 # Selenium setup
 options = Options()
@@ -26,12 +37,11 @@ if not SHOW_BROWSER:
 driver = webdriver.Chrome(service=Service(
     '/usr/local/bin/chromedriver'), options=options)
 
-# Drop collections
-NewsCollection.drop_collection()
-NewsCollectionNotPosted.drop_collection()
+
+def format_html(content):
+    return ''.join(BeautifulSoup(content, 'html.parser').stripped_strings)
 
 
-# Function to collect news index
 def collect_news_index():
     url = "https://www.gov.br/anatel/pt-br/assuntos/noticias"
     driver.get(url)
@@ -48,14 +58,15 @@ def collect_news_index():
             news_data['anatel_URL'] = ''
 
         try:
-            news_data['anatel_Titulo'] = news.find_element(
-                By.CSS_SELECTOR, 'div.conteudo > h2 > a').get_attribute('innerHTML')
+            news_data['anatel_Titulo'] = format_html(news.find_element(
+                By.CSS_SELECTOR, 'div.conteudo > h2 > a').get_attribute('innerHTML'))
         except Exception as e:
             news_data['anatel_Titulo'] = ''
+            print(f"Error collecting title at index {index}: {e}")
 
         try:
-            news_data['anatel_SubTitulo'] = news.find_element(
-                By.CSS_SELECTOR, 'div.conteudo > div.subtitulo-noticia').get_attribute('innerHTML')
+            news_data['anatel_SubTitulo'] = format_html(news.find_element(
+                By.CSS_SELECTOR, 'div.conteudo > div.subtitulo-noticia').get_attribute('innerHTML'))
         except:
             news_data['anatel_SubTitulo'] = ''
 
@@ -66,8 +77,8 @@ def collect_news_index():
             news_data['anatel_ImagemChamada'] = ''
 
         try:
-            news_data['anatel_Descricao'] = news.find_element(
-                By.CSS_SELECTOR, 'div.conteudo > span.descricao').get_attribute('innerHTML')
+            news_data['anatel_Descricao'] = format_html(news.find_element(
+                By.CSS_SELECTOR, 'div.conteudo > span > span.data').get_attribute('innerHTML'))
         except:
             news_data['anatel_Descricao'] = ''
 
@@ -76,20 +87,19 @@ def collect_news_index():
     return news_list
 
 
-# Function to collect news details
 def collect_news_details(news_url):
     driver.get(news_url)
     news_details = {}
 
     try:
         news_details['anatel_DataPublicacao'] = driver.find_element(
-            By.CSS_SELECTOR, '#plone-document-byline > span.documentPublished').text
+            By.CSS_SELECTOR, '#plone-document-byline > span.documentPublished').text.strip()
     except:
         news_details['anatel_DataPublicacao'] = ''
 
     try:
         news_details['anatel_DataAtualizacao'] = driver.find_element(
-            By.CSS_SELECTOR, '#plone-document-byline > span.documentModified').text
+            By.CSS_SELECTOR, '#plone-document-byline > span.documentModified').text.strip()
     except:
         news_details['anatel_DataAtualizacao'] = ''
 
@@ -100,28 +110,18 @@ def collect_news_details(news_url):
         news_details['anatel_ImagemPrincipal'] = ''
 
     try:
-        news_details['anatel_TextMateria'] = driver.find_element(
-            By.CSS_SELECTOR, '#parent-fieldname-text > div').get_attribute('innerHTML')
+        news_details['anatel_TextMateria'] = format_html(driver.find_element(
+            By.CSS_SELECTOR, '#parent-fieldname-text > div').get_attribute('innerHTML'))
     except:
         news_details['anatel_TextMateria'] = ''
 
     try:
         news_details['anatel_Categoria'] = driver.find_element(
-            By.CSS_SELECTOR, '#form-widgets-categoria').get_attribute('innerHTML')
+            By.CSS_SELECTOR, '#form-widgets-categoria').text.strip()
     except:
         news_details['anatel_Categoria'] = ''
 
     return news_details
-
-
-# Function to validate news data
-def is_valid_news(news_data):
-    required_fields = ['anatel_URL', 'anatel_Titulo', 'anatel_SubTitulo', 'anatel_ImagemChamada', 'anatel_Descricao',
-                       'anatel_DataPublicacao', 'anatel_ImagemPrincipal', 'anatel_TextMateria', 'anatel_Categoria']
-    for field in required_fields:
-        if not news_data.get(field):
-            return False
-    return True
 
 
 # Collect news from index
@@ -132,23 +132,21 @@ for news in news_index:
     details = collect_news_details(news['anatel_URL'])
     news.update(details)
 
-    if is_valid_news(news):
-        existing_news = NewsCollection.objects(
-            anatel_URL=news['anatel_URL']).first()
-        if existing_news:
-            if existing_news.anatel_DataAtualizacao != news['anatel_DataAtualizacao']:
-                existing_news.update(**news)
-        else:
-            news_doc = NewsCollection(**news)
-            news_doc.save()
+# Save news to MongoDB
+for news in news_index:
+    existing_news = news_collection.find_one(
+        {'anatel_URL': news['anatel_URL']})
+    if existing_news:
+        if existing_news['anatel_DataAtualizacao'] != news['anatel_DataAtualizacao']:
+            news_collection.update_one(
+                {'anatel_URL': news['anatel_URL']}, {'$set': news})
     else:
-        existing_news = NewsCollectionNotPosted.objects(
-            anatel_URL=news['anatel_URL']).first()
-        if existing_news:
-            existing_news.update(**news)
+        required_fields = ['anatel_URL', 'anatel_Titulo', 'anatel_SubTitulo', 'anatel_ImagemChamada', 'anatel_Descricao',
+                           'anatel_DataPublicacao', 'anatel_ImagemPrincipal', 'anatel_TextMateria', 'anatel_Categoria']
+        if all(news.get(field) for field in required_fields):
+            news_collection.insert_one(news)
         else:
-            news_doc = NewsCollectionNotPosted(**news)
-            news_doc.save()
+            news_collection_not_posted.insert_one(news)
 
 # Close the Selenium driver
 driver.quit()
