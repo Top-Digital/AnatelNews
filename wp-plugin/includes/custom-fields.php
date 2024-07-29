0f42c448113cab3d9749d2b724ea2a9334a83dcb
Adriano Alves Dal Cin Costa <adriano@ceviu.com.br>
2024-07-29 18:01:04 -0300

    salvando... configurando portas para rodar em produção, mas vou fazer um teste local antes v10

--- wp-plugin/includes/custom-fields.php
+++ wp-plugin/includes/custom-fields.php
@@ -17,11 +17,12 @@ function anatelnews_custom_box_html($post) {
         'anatel_ImagemChamada' => 'Imagem Chamada',
         'anatel_Descricao' => 'Descrição',
         'anatel_DataPublicacao' => 'Data de Publicação',
+        'anatel_DataAtualizacao' => 'Data de Atualização',
         'anatel_ImagemPrincipal' => 'Imagem Principal',
         'anatel_TextMateria' => 'Texto Matéria',
         'anatel_Categoria' => 'Categoria'
     ];
-    
+
     foreach ($fields as $field => $label) {
         $value = get_post_meta($post->ID, $field, true);
         echo "<label for='$field'>$label</label>";
@@ -39,14 +40,18 @@ function anatelnews_save_postdata($post_id) {
         'anatel_ImagemChamada',
         'anatel_Descricao',
         'anatel_DataPublicacao',
+        'anatel_DataAtualizacao',
         'anatel_ImagemPrincipal',
         'anatel_TextMateria',
         'anatel_Categoria'
     ];
-    
+
     foreach ($fields as $field) {
         if (array_key_exists($field, $_POST)) {
             update_post_meta($post_id, $field, $_POST[$field]);
         }
     }
 }
+
+add_action('add_meta_boxes', 'anatelnews_add_custom_box');
+add_action('save_post', 'anatelnews_save_postdata');
--- wp-plugin/includes/display-fields.php
+++ wp-plugin/includes/display-fields.php
@@ -1,21 +1,21 @@
 <?php
 // Exibir campos personalizados no frontend do WordPress
-function anatelnews_display_custom_fields() {
-    global $post;
-
-    // Verifica se é um post do tipo 'post' e se está em uma categoria específica
-    if (is_single() && 'post' == get_post_type($post->ID)) {
-        $custom_fields = get_post_meta($post->ID);
+function anatelnews_display_custom_fields($content) {
+    if (is_single() && 'post' == get_post_type()) {
+        $custom_fields = get_post_meta(get_the_ID());
 
         if (!empty($custom_fields)) {
-            echo '<div class="custom-fields">';
+            $custom_content = '<div class="custom-fields">';
             foreach ($custom_fields as $key => $value) {
                 if (strpos($key, 'anatel_') !== false) {
-                    echo '<p><strong>' . esc_html($key) . ':</strong> ' . esc_html($value[0]) . '</p>';
+                    $custom_content .= '<p><strong>' . esc_html($key) . ':</strong> ' . esc_html($value[0]) . '</p>';
                 }
             }
-            echo '</div>';
+            $custom_content .= '</div>';
+            $content .= $custom_content;
         }
     }
+
+    return $content;
 }
-add_action('the_content', 'anatelnews_display_custom_fields');
+add_filter('the_content', 'anatelnews_display_custom_fields');
