b6b2d0442a915e3ac52fb48bbae14b013f25e353
Adriano Alves Dal Cin Costa <adriano@ceviu.com.br>
2024-08-02 06:32:24 -0300

    subindo dois pacotes com versões mais atualiadas e alterei o shell para retirar verificação de docker

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-# Nome do container Docker do WordPress
-CONTAINER_NAME="docker_wordpress_1" # Altere para o nome correto do seu container
+# Diretório local onde os arquivos do plugin estão localizados
+LOCAL_PLUGIN_DIR="/home/adriano/dev/AnatelNews/wp-plugin"
 
-# Diretório no container onde os plugins do WordPress estão localizados
-CONTAINER_PLUGIN_DIR="/var/www/html/wp-content/plugins/anatelnews"
+# Nome do arquivo .zip do plugin
+PLUGIN_ZIP="anatelnews.zip"
 
-# Diretório local onde os novos arquivos do plugin estão localizados
-LOCAL_PLUGIN_DIR="../wp-plugin" # Caminho relativo ao diretório shell
+# Diretório de destino para o arquivo .zip
+DEST_DIR="$HOME/Downloads"
 
-# Verifica se o container está em execução
-if [ $(docker ps -q -f name=${CONTAINER_NAME}) ]; then
-  echo "Container ${CONTAINER_NAME} está em execução. Continuando com a implantação..."
-else
-  echo "Container ${CONTAINER_NAME} não está em execução. Certifique-se de que o container Docker esteja em execução."
-  exit 1
+# Verifica se o diretório de destino existe
+if [ ! -d "$DEST_DIR" ]; then
+  echo "O diretório de destino não existe. Criando $DEST_DIR..."
+  mkdir -p "$DEST_DIR"
 fi
 
-# Apagar todos os arquivos e diretórios dentro de anatelnews no container
-echo "Apagando arquivos antigos no container..."
-docker exec -it ${CONTAINER_NAME} rm -rf ${CONTAINER_PLUGIN_DIR}/*
+# Navegar para o diretório onde os arquivos do plugin estão localizados
+cd "$LOCAL_PLUGIN_DIR" || {
+  echo "Erro ao acessar o diretório $LOCAL_PLUGIN_DIR"
+  exit 1
+}
 
-# Copiar todos os arquivos e diretórios do wp-plugin para o diretório anatelnews no container
-echo "Copiando novos arquivos para o container..."
-docker cp ${LOCAL_PLUGIN_DIR}/. ${CONTAINER_NAME}:${CONTAINER_PLUGIN_DIR}/
+# Criar o arquivo .zip do plugin
+echo "Criando arquivo .zip do plugin..."
+zip -r "$PLUGIN_ZIP" ./*
+
+# Mover o arquivo .zip para o diretório de downloads
+echo "Movendo o arquivo .zip para $DEST_DIR..."
+mv "$PLUGIN_ZIP" "$DEST_DIR"
 
 echo "Implantação concluída com sucesso!"
