da18cbc3a5bb27ab22fe1ed4a07740dc0d852440
Adriano Alves Dal Cin Costa <adriano@ceviu.com.br>
2024-08-04 11:09:40 -0300

    atualizando no git com a library XML-RPC do python para WP

--- wp-plugin/includes/api-integration.php
+++ wp-plugin/includes/api-integration.php
@@ -1,38 +1,29 @@
 <?php
-// Função para registrar endpoint customizado
-function anatelnews_register_api_endpoints() {
-    register_rest_route('anatelnews/v1', '/create-post', array(
-        'methods' => 'POST',
-        'callback' => 'anatelnews_create_post',
-        'permission_callback' => '__return_true'
+function anatelnews_fetch_posts() {
+    $webhook_token = get_option('anatelnews_webhook_token');
+    $api_url = 'http://localhost:5000/getnewposts';
+
+    $response = wp_remote_get($api_url, array(
+        'headers' => array(
+            'Authorization' => 'Bearer ' . $webhook_token
+        )
     ));
-}
-add_action('rest_api_init', 'anatelnews_register_api_endpoints');
 
-// Função de callback para criar post
-function anatelnews_create_post($data) {
-    $selected_category = get_option('anatelnews_category');
-    if (!$selected_category) {
-        return new WP_Error('category_not_selected', 'Nenhuma categoria selecionada no plugin', array('status' => 400));
+    if (is_wp_error($response)) {
+        return;
     }
 
-    $post_data = array(
-        'post_title'    => $data['title'],
-        'post_content'  => $data['content'],
-        'post_status'   => 'publish',
-        'post_type'     => 'post',
-        'post_date'     => $data['meta']['anatel_DataPublicacao'], // Use a data de publicação da Anatel
-        'post_category' => array($selected_category), // Adicionar a categoria selecionada
-        'meta_input'    => $data['meta'] // Inserir todos os metadados de uma vez
-    );
+    $posts = json_decode(wp_remote_retrieve_body($response), true);
 
-    // Inserir o post no WordPress
-    $post_id = wp_insert_post($post_data);
-
-    if (is_wp_error($post_id)) {
-        return new WP_Error('post_creation_failed', 'Failed to create post', array('status' => 500));
+    foreach ($posts as $post) {
+        if ($post['action'] === 'delete') {
+            // Lógica para deletar o post
+        } elseif ($post['action'] === 'update') {
+            // Lógica para atualizar o post
+        } else {
+            // Lógica para criar o post
+        }
     }
-
-    return new WP_REST_Response($post_data, 200);
 }
+add_action('init', 'anatelnews_fetch_posts');
 ?>
